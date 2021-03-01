47a2c865efbdb2f510601c8bba5118dbe8e80327
kreusada <67752638+kreusada@users.noreply.github.com>
2021-03-01 18:36:36 +0000

    fix

--- marvel/decorators.py
+++ marvel/decorators.py
@@ -1,15 +1,10 @@
 from redbot.core import commands, Config
 
-from .abc import MixinMeta
-
-class Pred(MixinMeta):
-    pass
-
-    def limit_guilds():
-        async def pred(ctx):
-            allowed = await self.config.allowlist()
-            g = ctx.guild.id
-            if oct(g) not in allowed:
-                return False
-            return True
-        return commands.check(pred)
+def limit_guilds():
+    async def pred(ctx):
+        allowed = await self.config.allowlist()
+        g = ctx.guild.id
+        if oct(g) not in allowed:
+            return False
+        return True
+    return commands.check(pred)
